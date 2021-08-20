513db41f1bebcce37829cb471e7c67e2d395de50
Xinshuoo <huangxinshuo@live.com>
2021-08-20 14:11:21 +0800

    Add plusminus, percent functionality

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const historyDisplayBox = document.querySelector(".history");
 const operators = document.querySelectorAll(".operator");
 const digits = document.querySelectorAll(".digit");
 const ac = document.querySelector("#ac");
+const decimal = document.querySelector("#decimal");
+const percent = document.querySelector("#percent");
+const plusMinus = document.querySelector("#plus-minus");
 
 // Add EventListeners
 digits.forEach((button) => 
@@ -13,6 +16,9 @@ operators.forEach((button) =>
     button.addEventListener("click", () => addOperator(button.textContent))
 );
 ac.addEventListener("click", () => clearAll());
+decimal.addEventListener("click", () => addToInput(decimal.textContent));
+percent.addEventListener("click", () => addToInput(percent.textContent));
+plusMinus.addEventListener("click", () => addToInput(plusMinus.textContent));
 
 // Clear everything
 function clearAll() {
@@ -28,17 +34,39 @@ function addToInput(digit) {
     if (inputNumber === null) {
         inputNumber = "";
     }
-    inputNumber.toString();
+    if (digit === "±") {
+        if (inputNumber === "") { // "±" is the first button clicked on
+            digit = "-";
+        }    
+        else {
+            inputNumber = -inputNumber; // plus-minus symbol will toggle between +ve and -ve
+            updateInputDisplayBox(inputNumber);
+            return;
+        }
+    }
+    if (digit === "%" && inputNumber !== "") {
+        inputNumber = inputNumber / 100;
+        updateInputDisplayBox(inputNumber);
+        return;
+    }
+    if (inputNumber.toString().includes(".") && digit === ".") { // number already has a decimal place
+        digit = "";
+    }
     inputNumber += digit;
-    inputNumber = parseFloat(inputNumber)
+
+    // only convert to float if no decimal point (otherwise "3." becomes "3")
+    // only convert to float if input is NOT just "-" (due to the plus-minus toggle)
+    if (!digit === "." && !digit === "-") { 
+        inputNumber = parseFloat(inputNumber);
+    }
     updateInputDisplayBox(inputNumber);
 }
 function updateInputDisplayBox(num) {
-    if (num < 1 && num.toString().length > 5) {
-        num = parseFloat(num).toExponential(3);
+    if (num < 1 && num.toString().length > 6) {
+        num = parseFloat(num).toExponential(4);
     } 
-    else if (num.toString().length > 5) {
-        num = num.toPrecision(4);
+    else if (num.toString().length > 6) {
+        num = parseFloat(num).toPrecision(5);
     }
     inputDisplayBox.textContent = num;
 }
@@ -66,8 +94,17 @@ function addOperator(operator) {
         
          // show the result of the calculation with the previous operator (which is 
          // already displayed in history)
-         result = operate(currentOperatorArray[previousOperator], previousNumber, inputNumber);
+         result = operate(currentOperatorArray[previousOperator], parseFloat(previousNumber), parseFloat(inputNumber));
          updateInputDisplayBox(result);
+
+         if (operator === "=") { // if "=" chosen, prepare to start a new calculation
+            updateHistoryDisplayBox(inputNumber, operator);           
+            historyExpression = ""; // make sure history is clear for the next calculation
+            operatorSelected = false;
+            previousNumber = inputNumber = result;
+            previousOperator = "";
+            return;
+         }
 
     }
     
